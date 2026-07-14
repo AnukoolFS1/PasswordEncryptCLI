ccaf85db3f9f99893afd4763f46b6e4283aed135
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-07-14 20:47:37 +0530

    setting up files

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	// entries := basic()
 
 	// fmt.Println()
-	// result, err := saveData(entries)
+	// result, err := SaveData(entries)
 
 	// if err != nil {
 	// 	panic(err)
@@ -29,46 +29,9 @@ func main() {
 
 }
 
-func basic() []Entry {
-	var entries []Entry
 
-	entry := Entry{
-		"Git", "Anukool", "123456",
-	}
 
-	entries = append(entries, entry)
 
-	fmt.Println(entries)
-
-	var service string
-	var username string
-	var password string
-
-	fmt.Println("Servie: ")
-	fmt.Scanln(&service)
-
-	fmt.Println("Username: ")
-	fmt.Scanln(&username)
-
-	fmt.Println("Password: ")
-	fmt.Scanln(&password)
-
-	entry = Entry{Service: service, Username: username, Password: password}
-	entries = append(entries, entry)
-
-	return entries
-}
-
-func saveData(entries []Entry) (string, error) {
-	data, _ := json.Marshal(entries)
-
-	err := os.WriteFile("password.json", data, 0644)
-
-	if err != nil {
-		return "", err
-	}
-	return "File has been saved", nil //errors.New("Something wrong")
-}
 
 func RetrieveData() []Entry {
 	data, err := os.ReadFile("password.json")
