6c026598aff6395be2a8b6e60ebd8e9c0bdbdd07
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-08-16 15:45:41 +0530

    correction in other args and confirmpassword functions

--- cli.go
+++ cli.go
@@ -1,60 +1,59 @@
 package main
 
-// import (
-// 	"fmt"
-// )
+import (
+	"fmt"
+)
 
-// var MasterPassword = "IamOwner"
+var MasterPassword = "IamOwner"
 
-// func CheckArgs(args []string) bool {
-// 	if len(args) < 2 {
-// 		fmt.Println("Usage:")
-// 		fmt.Println("add [service] [username] [password]")
-// 		fmt.Println("update [username] [password]")
-// 		fmt.Println("delete [username]")
-// 		fmt.Println("get [username]")
-// 		fmt.Println("list")
-// 		return false
-// 	}
+func CheckArgs(args []string) bool {
+	if len(args) < 2 {
+		fmt.Println("Usage:")
+		fmt.Println("add [service] [username] [password]")
+		fmt.Println("update [username] [password]")
+		fmt.Println("delete [username]")
+		fmt.Println("get [username]")
+		fmt.Println("list")
+		return false
+	}
 
-// 	return true
-// }
+	return true
+}
 
-// func ArgsFunc(args []string) {
-// 	switch args[1] {
-// 	case "add":
-// 		HandleAdd(args)
+func ArgsFunc(args []string, Mpassword string) {
+	switch args[1] {
+	case "add":
+		HandleAdd(args, Mpassword)
 
-// 	case "get":
-// 		HandleGet(args)
+	case "get":
+		HandleGet(args, Mpassword)
 
-// 	case "list":
-// 		HandleList(args)
+	case "list":
+		HandleList(args, Mpassword)
 
-// 	case "delete":
-// 		HandleDelete(args)
+	case "delete":
+		HandleDelete(args, Mpassword)
 
-// 	case "update":
-// 		HandleUpdate(args)
+	case "update":
+		HandleUpdate(args, Mpassword)
 
-// 	case "create-db":
-// 		CreateDb()
-// 		fmt.Println("Database has been created.")
+	case "create-db":
+		CreateDb()
+		fmt.Println("Database has been created.")
 
-// 	default:
-// 		fmt.Println("Command not found")
-// 	}
-// }
+	default:
+		fmt.Println("Command not found")
+	}
+}
 
-// func ConfirmMaster() bool {
-// 	var password string
-// 	fmt.Println("Enter the master password")
-// 	fmt.Scanln(&password)
+func ConfirmMaster() (string, bool) {
+	var password string
+	fmt.Println("Enter the master password")
+	fmt.Scanln(&password)
 
-// 	if password != MasterPassword {
-// 		fmt.Println("You are not authorised")
-// 		return false
-// 	}
-// 	return true
-// }
-
+	if password != MasterPassword {
+		fmt.Println("You are not authorised")
+		return "", false
+	}
+	return password, true
+}
